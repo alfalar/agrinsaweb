97abe00639ba983e2f01338707a1c2816fc267d2
ALFREDO ALARCON <alfredo.alarcon@sigavl.com>
2013-04-12 16:33:01 -0500

    dia 3

--- agrinsa/src/co/geographs/agrinsa/beans/ConsultasBean.java
+++ agrinsa/src/co/geographs/agrinsa/beans/ConsultasBean.java
@@ -1,11 +1,12 @@
 package co.geographs.agrinsa.beans;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.event.ValueChangeEvent;
+import javax.faces.model.SelectItem;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,47 +14,88 @@ import org.springframework.security.core.userdetails.UserDetails;
 import co.geographs.agrinsa.dao.ConsultasDao;
 import co.geographs.agrinsa.dao.business.Areaxciudad;
 import co.geographs.agrinsa.dao.business.TiposConsulta;
+import co.geographs.agrinsa.util.FacesUtil;
 import co.geographs.agrinsa.util.SpringUtils;
 
 @ManagedBean(name = "consultasBean")
 @SessionScoped
 public class ConsultasBean implements Serializable {
-	private List<TiposConsulta> tiposconsulta;
+	private List<SelectItem> tiposconsulta;
 	private List<Areaxciudad> areaxciudad;
-	private TiposConsulta selectedTiposconsulta;
-
-	public void cambioconsulta(){
-		System.out.println("*******************************:"+selectedTiposconsulta.getValue());
+	private String selectedTiposconsulta;
+	private boolean areaxciudadenabled=false;
+	public void cambioconsulta() {
+		System.out.println("*******************************:"
+				+ selectedTiposconsulta);
+		if(selectedTiposconsulta.equalsIgnoreCase("areaxciudad")){
+			areaxciudadenabled=true;
+		}else{
+			areaxciudadenabled=false;
+		}
+		
 	}
+	
+	public void setTiposconsulta(ArrayList<SelectItem> tiposconsulta) {
+		this.tiposconsulta = tiposconsulta;
+	}
+
+
+
 	/**
 	 * Retorna la lista de tipos de consulta
 	 * 
 	 * @return
 	 */
-	public List<TiposConsulta> getTiposconsulta() {
-		UserDetails userDetails = (UserDetails) SecurityContextHolder
-				.getContext().getAuthentication().getPrincipal();
-		ConsultasDao consultasDao = (ConsultasDao) SpringUtils
-				.getBean("consultasDao");
-		tiposconsulta = consultasDao.getConsultas(userDetails.getUsername());
-		return tiposconsulta;
+	public List<SelectItem> getTiposconsulta() {
+		try {
+			UserDetails userDetails = (UserDetails) SecurityContextHolder
+					.getContext().getAuthentication().getPrincipal();
+			ConsultasDao consultasDao = (ConsultasDao) SpringUtils
+					.getBean("consultasDao");
+			List l = consultasDao.getConsultas(userDetails.getUsername());
+			tiposconsulta = (ArrayList) FacesUtil.entityToSelectItem(l,
+					"getValue", "getLabel");
+			return tiposconsulta;
+		} catch (Exception e) {
+			return null;
+		}
 	}
+
 	/**
 	 * Consulta1: Area x ciudad
+	 * 
 	 * @return
 	 */
 	public List<Areaxciudad> getAreaxciudad() {
 		ConsultasDao consultasDao = (ConsultasDao) SpringUtils
 				.getBean("consultasDao");
-		areaxciudad=consultasDao.getAreaxciudad();		
+		areaxciudad = consultasDao.getAreaxciudad();
 		return areaxciudad;
 	}
-	public TiposConsulta getSelectedTiposconsulta() {
+
+	
+
+
+	public String getSelectedTiposconsulta() {
 		return selectedTiposconsulta;
 	}
-	public void setSelectedTiposconsulta(TiposConsulta selectedTiposconsulta) {
+
+
+
+	public void setSelectedTiposconsulta(String selectedTiposconsulta) {
 		this.selectedTiposconsulta = selectedTiposconsulta;
 	}
-	
-	
+
+
+
+	public boolean isAreaxciudadenabled() {
+		return areaxciudadenabled;
+	}
+
+
+
+	public void setAreaxciudadenabled(boolean areaxciudadenabled) {
+		this.areaxciudadenabled = areaxciudadenabled;
+	}
+
 }
--- agrinsa/src/co/geographs/agrinsa/util/FacesUtil.java
+++ agrinsa/src/co/geographs/agrinsa/util/FacesUtil.java
@@ -1,7 +1,12 @@
 package co.geographs.agrinsa.util;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,4 +22,30 @@ public class FacesUtil {
 		session.invalidate();
 	}
 
+	public static List<SelectItem> entityToSelectItem(List<?> list,
+            String getIdMethod, String getDescMethod) throws Exception {
+     List<SelectItem> items = new ArrayList<SelectItem>();
+
+     Method idMethod = null;
+     Method descMethod = null;
+
+     for (int index = 0; index < list.size(); index++) {
+            Object item = list.get(index);
+            // On the first run, initialize reflection methods for object
+            if (idMethod == null) {
+                   Class<? extends Object> obj = item.getClass();
+                   idMethod = obj.getMethod(getIdMethod, new Class[] {});
+                   descMethod = obj.getMethod(getDescMethod, new Class[] {});
+            }
+            // invoke Methods
+            String id = (String) idMethod.invoke(item, new Object[] {});
+            String name = (String) descMethod.invoke(item, new Object[] {});            
+            SelectItem selectItem = new SelectItem();            
+            selectItem.setLabel(name);
+            selectItem.setValue(id.toString());
+            items.add(selectItem);
+     }
+
+     return items;
+}
 }
